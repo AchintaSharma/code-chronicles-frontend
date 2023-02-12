4aa9f5a35422a2d4000e5137947f009c3d0b7760
Achinta Sharma <23achinta@gmail.com>
2023-02-12 18:16:28 +0530

    init frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div>Hello BlogApp</div>
+			<Button variant="contained">Click Me!</Button>
+		</>
+	);
+}
+export default App;
